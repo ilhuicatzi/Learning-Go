1fd56ccdeb653a37b027e419f5eb726339354bd5
g.ilhuicatzi <g.ilhuicatzi@gmail.com>
2026-06-16 22:48:34 -0600

    feat: create project home page and documentation modules for Go fundamentals

---
+++ src/app/go-rest-api/page.tsx
@@ -0,0 +1,14 @@
+import RestApi from "@/pages/home/RestApi"
+
+export const metadata = {
+    title: 'API REST con CRUD en Go | Learning Go',
+    description: 'Aprende a construir una API REST completa con operaciones CRUD y JSON en Go.',
+}
+
+function GoRestApi() {
+    return (
+        <RestApi />
+    )
+}
+
+export default GoRestApi
