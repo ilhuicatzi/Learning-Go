940537443e5703a7cf4c73ef0a988edc4cc9dd7b
g.ilhuicatzi <g.ilhuicatzi@gmail.com>
2026-06-16 22:48:23 -0600

    feat: add educational modules for Go fundamentals, including pointers, structs, functions, and REST API implementation

---
+++ src/app/go-pointers-structs/page.tsx
@@ -0,0 +1,14 @@
+import PointersStructs from "@/pages/home/PointersStructs"
+
+export const metadata = {
+    title: 'Punteros y Estructuras en Go | Learning Go',
+    description: 'Aprende sobre punteros, estructuras y receptores de métodos en Go.',
+}
+
+function GoPointersStructs() {
+    return (
+        <PointersStructs />
+    )
+}
+
+export default GoPointersStructs
