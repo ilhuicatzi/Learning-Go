61aa7cf5f851143dbc44ad34bfedfc6e65c90f95
g.ilhuicatzi <g.ilhuicatzi@gmail.com>
2026-06-16 22:48:05 -0600

    feat: scaffold learning modules for Go concepts including APIs, concurrency, interfaces, and pointers

---
+++ src/app/go-interfaces/page.tsx
@@ -0,0 +1,14 @@
+import Interfaces from "@/pages/home/Interfaces"
+
+export const metadata = {
+    title: 'Interfaces en Go | Learning Go',
+    description: 'Aprende sobre el desacoplamiento, la implementación implícita y aserciones de tipo en Go.',
+}
+
+function GoInterfaces() {
+    return (
+        <Interfaces />
+    )
+}
+
+export default GoInterfaces
